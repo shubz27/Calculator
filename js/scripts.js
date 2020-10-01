ea79cfcfc725284768a45824adbe24030c79c383
panks2209 <72216326+panks2209@users.noreply.github.com>
2020-10-01 22:11:43 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
-var add = function(number1, number2) {
+let add = function(number1, number2) {
   return number1 + number2;
 };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+let number1 = parseInt(prompt("Enter a number:"));
+let number2 = parseInt(prompt("Enter another number:"));
 alert(add(number1, number2));
-var subtract = function(number1, number2) {
+let subtract = function(number1, number2) {
   return number1 - number2;
 };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+let number1 = parseInt(prompt("Enter a number:"));
+let number2 = parseInt(prompt("Enter another number:"));
 alert(subtract(number1, number2));
-var multiply = function(number1, number2) {
+let multiply = function(number1, number2) {
   return number1 * number2;
 };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+let number1 = parseInt(prompt("Enter a number:"));
+let number2 = parseInt(prompt("Enter another number:"));
 alert(multiply(number1, number2));
-var divide = function(number1, number2) {
+let divide = function(number1, number2) {
   return number1 / number2;
 };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+let number1 = parseInt(prompt("Enter a number:"));
+let number2 = parseInt(prompt("Enter another number:"));
 alert(divide(number1, number2));
